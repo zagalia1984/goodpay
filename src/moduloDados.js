01e6d8221b5a884e814ba81172c507ba9f90616b
Diego Zagalia Ferreira <diegozagalia@hotmail.com>
2021-12-22 09:37:47 -0300

    Alterações arquivos

--- src/moduloDados.js
+++ src/moduloDados.js
@@ -1,30 +1,16 @@
 const userLogin = [
-    {id: 1001, username:"victoricoma", senha:"gordinho123", email: "victor.icoma@soulcodeacademy.org"},
-    {id: 1002, username:"marcelofarofa", senha:"ff123456", email: "marceloff@gmail.com"},
-];
-
-export default userLogin;
-
-// document.getElementById("titleModal").innerHTML="Campos Vazios";
-// document.getElementById("bodyModal").innerHTML="Os campos não podem estar vazios!";
-// document.getElementById("btnModalClose").innerHTML="Fechar";
-// document.getElementById("btnModalSave").innerHTML="Salvar";
-
-
-const comunicacaoModal = [
-    {idModal: "camposVazios1", 
-    title: "Campos Vazios",
-    bodyModal: "Os campos não podem estar vazios!",
-    b1: "Fechar",
-    b2: "Salvar"},
-    {idModal: "usuarioNaoExiste1", 
-    title: "Não encontramos o usuário ",
-    bodyModal: "Não é possível entrar o usuário solicitado!",
-    b1: "Fechar",
-    b2: "Criar"},
-    {idModal: "camposVazios1", 
-    title: "Parabéns, vc está logado!",
-    bodyModal: "As informações foram confirmadas,!",
-    b1: "Fechar",
-    b2: "Salvar"},
-]
+    {
+      id: 1001,
+      username: 'victoricoma',
+      senha: 'gordinho123',
+      email: 'victoricoma@soulcodeacademy.org'
+    },
+    {
+      id: 1002,
+      username: 'marcelofarofa',
+      senha: 'ff123456',
+      email: 'marceloff@gmail.com'
+    }
+  ]
+  
+  export default userLogin
